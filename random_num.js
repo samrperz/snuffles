002981dd797b2454aaaf3a50ca02d93e7ab9f1da
unknown <srperz@EHS028270.ACPS.SBAC.EDU>
2016-02-25 12:35:16 -0500

    Random Number Generator Switch Edition

--- random_num.js
+++ random_num.js
@@ -1,21 +1,50 @@
-function roll4(){
-
+var int;
+var x = 1;
+function roll4() {
+    int = Math.ceil(Math.random() * 4);
+    document.getElementById("result").value = int;
+    
 }
-function roll6(){
-
+function roll6() {
+    int = Math.ceil(Math.random() * 6);
+    document.getElementById("result").value = int;
 }
-function roll8(){
-
+function roll8() {
+    int = Math.ceil(Math.random() * 8);
+    document.getElementById("result").value = int;
 }
-function roll10(){
-
+function roll10() {
+    int = Math.ceil(Math.random() * 10);
+    document.getElementById("result").value = int;
 }
-function roll12(){
-
+function roll12() {
+    int = Math.ceil(Math.random() * 12);
+    document.getElementById("result").value = int;
 }
-function roll20(){
-
+function roll20() {
+    int = Math.ceil(Math.random() * 20);
+    document.getElementById("result").value = int;
 }
-function roll100(){
+function roll100() {
+    int = Math.ceil(Math.random() * 100);
+    document.getElementById("result").value = int;
+}
+function rollClear() {
+    document.getElementById("result").value = "";
+}
 
+
+
+function rollMega(diceValue, numberOfDice) {
+    switch(diceValue) {
+        case 4:
+            int += Math.ceil(Math.random() * 4);
+            document.getElementById("result").value = int;
+            break;
+        case 6:
+            int += Math.ceil(Math.random()*6);
+            document.getElementById("result").value = int;
+            break;
+        case 8:
+    }
 }
