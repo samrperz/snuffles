0ce19ac200026e378921dbd039e70111abc37ef6
unknown <crbenton@EHS028267.ACPS.SBAC.EDU>
2016-02-25 12:52:39 -0500

    Dice Generator Working

--- random_num.js
+++ random_num.js
@@ -1,50 +1,40 @@
 var int;
-var x = 1;
-function roll4() {
-    int = Math.ceil(Math.random() * 4);
-    document.getElementById("result").value = int;
-    
-}
-function roll6() {
-    int = Math.ceil(Math.random() * 6);
-    document.getElementById("result").value = int;
-}
-function roll8() {
-    int = Math.ceil(Math.random() * 8);
-    document.getElementById("result").value = int;
-}
-function roll10() {
-    int = Math.ceil(Math.random() * 10);
-    document.getElementById("result").value = int;
-}
-function roll12() {
-    int = Math.ceil(Math.random() * 12);
-    document.getElementById("result").value = int;
-}
-function roll20() {
-    int = Math.ceil(Math.random() * 20);
-    document.getElementById("result").value = int;
-}
-function roll100() {
-    int = Math.ceil(Math.random() * 100);
-    document.getElementById("result").value = int;
-}
 function rollClear() {
     document.getElementById("result").value = "";
 }
 
+function rollCounter(diceValue,numberOfDice) {
+    var i = document.getElementById("numrolled").value;
+    do {
+        rollMega(diceValue);
+        i --;
+    } while(i > 0);
+    document.getElementById("result").value = int;
+    int = 0;
+}
 
-
-function rollMega(diceValue, numberOfDice) {
+function rollMega(diceValue) {
     switch(diceValue) {
         case 4:
             int += Math.ceil(Math.random() * 4);
-            document.getElementById("result").value = int;
             break;
         case 6:
             int += Math.ceil(Math.random()*6);
-            document.getElementById("result").value = int;
             break;
         case 8:
+            int += Math.ceil(Math.random()*8);
+            break;
+        case 10:
+            int += Math.ceil(Math.random()*10);
+            break;
+        case 12:
+            int += Math.ceil(Math.random()*12);
+            break;
+        case 20:
+            int += Math.ceil(Math.random()*20);
+            break;
+        case 100:
+            int += Math.ceil(Math.random()*100);
+            break;
     }
 }
