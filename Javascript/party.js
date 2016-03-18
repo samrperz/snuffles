f794870052e240715f03b8ee55bf76bcff3346bb
unknown <srperz@EHS028270.ACPS.SBAC.EDU>
2016-03-18 14:40:20 -0400

    txt stuf

--- Javascript/party.js
+++ Javascript/party.js
@@ -1,9 +1,8 @@
 //var textFile = 
 //var file = new File("party.txt");
-"use strict";
 var party = [];
 
-party[1] = {name: "", race: "", hp: "", ac: "", str: "", dex: "", con: "", int: "", wis: "", chr: "", xp: ""};
+party[] = {name: "", race: "", hp: "", ac: "", str: "", dex: "", con: "", int: "", wis: "", chr: "", xp: ""};
 
 function readFile() {
     
