2cb301653b254c1b401770e4c33a117e02697980
unknown <srperz@EHS028270.ACPS.SBAC.EDU>
2016-03-18 14:39:21 -0400

    txt stuf

---
+++ Javascript/party.js
@@ -0,0 +1,16 @@
+//var textFile = 
+//var file = new File("party.txt");
+"use strict";
+var party = [];
+
+party[1] = {name: "", race: "", hp: "", ac: "", str: "", dex: "", con: "", int: "", wis: "", chr: "", xp: ""};
+
+function readFile() {
+    
+}
+function displayParty() {
+    window.onload(readFile());
+}
+function newMember() {
+    
+}
--- Javascript/thaco_calc.js
+++ Javascript/thaco_calc.js
@@ -1,7 +1,5 @@
 function thacoCalc(){
     var x = 0 + parseInt(document.getElementById("thacoX").value);
     var y = 0 + parseInt(document.getElementById("armorY").value);
-    
     document.getElementById("answer").value = x - y;
-    
 }
